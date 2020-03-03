1b8487c2d1ed5d5865c155b115902b663e7ae99f
MANZANERO JIMENEZ MARIO <mmanji2@mdef.es>
2020-03-03 14:45:05 +0100

    Hecho casi toda la estructura

--- data.js
+++ data.js
@@ -1,7 +1,59 @@
 var request = require("request");
 var cheerio = require("cheerio");
-var fs = require('fs'); 
+var fs = require('fs');
 
+var proxyUrl = "http://" + "mmanji2" + ":" + "Minisdef03" + "@" + "10.7.180.112" + ":" + "80";
+
+var proxiedRequest = request.defaults({ 'proxy': proxyUrl });
+
+proxiedRequest.get("https://www.worldometers.info/coronavirus/", function(err, resp, body) {
+    var $ = cheerio.load(body);
+
+    $.fn.toJson = function() {
+
+        if (!this.is('table')) {
+            return;
+        }
+
+        var results = [],
+            headings = [];
+
+        this.find('thead tr th').each(function(index, value) {
+            headings.push($(value).text());
+        });
+
+
+        this.find('tbody tr').each(function(indx, obj) {
+            var row = {};
+            var tds = $(obj).children('td');
+            headings.forEach(function(key, index) {
+                var value = tds.eq(index).text();
+                row[key] = value;
+            });
+            results.push(row);
+        });
+
+        return results;
+    }
+
+    $(function() {
+        $('#results').html(JSON.stringify($('#excel').toJson(), null, '\t'));
+    });
+
+    var json = $('#main_table_countries').toJson();
+    console.log(json);
+
+    fs.writeFile('data/data.json', JSON.stringify(json), function(err) {
+        if (err) throw err;
+        console.log('Guardado');
+    });
+
+});
+
+
+
+
+/*
 request({
     uri: "https://www.worldometers.info/coronavirus/",
   }, function(error, response, body) {
@@ -21,4 +73,4 @@ request({
             console.log('Saved!');
         });
     }
-  });
+  });*/
