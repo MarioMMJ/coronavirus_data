cae39b178d40898f790d7bb7acb8307be71d0ed5
MANZANERO JIMENEZ MARIO <mmanji2@mdef.es>
2020-03-04 09:04:53 +0100

    Arreglado un error con los numeros que se pasaban al html

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,26 @@ var router = express.Router();
 var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var datos = fs.readFileSync("data/data.json");
-  var noticias;
-  var noticias = fs.readFileSync('data/noticias.json');
-  var noticias_json = JSON.parse(noticias);
-  var json = JSON.parse(datos);
-  var totales;
-  var d = new Date();
-  for(var i = 0; i < json.length; i++){
-    if(json[i]["Country,Other"].trim() == "Total:"){
-      totales = json[i];
+    var datos = fs.readFileSync("data/data.json");
+    var noticias;
+    var noticias = fs.readFileSync('data/noticias.json');
+    var noticias_json = JSON.parse(noticias);
+    var json = JSON.parse(datos);
+    var totales;
+    var d = new Date();
+    for (var i = 0; i < json.length; i++) {
+        if (json[i]["Country,Other"].trim() == "Total:") {
+            totales = json[i];
+        }
     }
-  }
-  res.render('index', { totales: totales, paises : json.length, estadisticasPaises : json, noticias: noticias_json, fecha: (d.getDay()+1) +"/" + (d.getMonth()+1) + "/" + d.getFullYear()});
+    totales["TotalCases"] = totales["TotalCases"].replace(",", "");
+    totales["NewCases"] = totales["NewCases"].replace(",", "");
+    totales["TotalDeaths"] = totales["TotalDeaths"].replace(",", "");
+    totales["NewDeaths"] = totales["NewDeaths"].replace(",", "");
+    totales["TotalRecovered"] = totales["TotalRecovered"].replace(",", "");
+    totales["ActiveCases"] = totales["ActiveCases"].replace(",", "");
+    totales["Serious,Critical"] = totales["Serious,Critical"].replace(",", "");
+    res.render('index', { totales: totales, paises: json.length, estadisticasPaises: json, noticias: noticias_json, fecha: (d.getDay() + 1) + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() });
 });
 
 module.exports = router;
