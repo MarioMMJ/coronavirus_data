fcd432b279f8c820fb8c1c66a7640450fffa9a4b
Bcct <xbacost@hotmail.com>
2020-03-04 20:24:54 +0100

    añadido social icons

--- public/js/all.js
+++ public/js/all.js
@@ -4,8 +4,8 @@ function randomNumber(min, max) {
 
 
 
-$(document).ready(function() {
-    $('.count').each(function() {
+$(document).ready(function () {
+    $('.count').each(function () {
         var $this = $(this),
             countTo = $this.attr('data-count');
         $({ countNum: $this.text() }).animate({
@@ -13,14 +13,58 @@ $(document).ready(function() {
         }, {
             duration: randomNumber(1000, 2000),
             easing: 'swing',
-            step: function() {
+            step: function () {
                 $this.text(Math.floor(this.countNum));
             },
-            complete: function() {
+            complete: function () {
                 $this.text(this.countNum);
                 //alert('finished');
             }
 
         });
     });
+
+
+
+    var social_open = false
+    window.addEventListener('load', () => {
+        const menu = document.querySelector('.social-button');
+
+        menu.addEventListener('click', () => {
+            const icon = document.querySelector('#social-icon');
+            if (social_open == true) {
+                social_open = false;
+
+                menu.title = menu.title.replace(/hide/, "expand")
+                menu.classList.remove('social-button-open')
+                icon.classList.remove('fa-times')
+                icon.classList.add('fa-share-alt')
+
+                var menu_point = document.querySelectorAll(".social-point");
+                for (let i = 0; i < menu_point.length; i++) {
+                    menu_point[i].classList.remove('social-point-open');
+                    setTimeout(function () {
+                        menu_point[i].hidden = true;
+                    }, 800)
+                }
+            } else {
+                social_open = true;
+
+                menu.title = menu.title.replace(/expand/, "hide")
+                menu.classList.add('social-button-open');
+                icon.classList.remove('fa-share-alt')
+                icon.classList.add('fa-times')
+
+                var menu_point = document.querySelectorAll(".social-point");
+                for (let i = 0; i < menu_point.length; i++) {
+                    menu_point[i].hidden = false;
+                    setTimeout(function () {
+                        menu_point[i].classList.add('social-point-open');
+                    }, 200)
+                }
+            }
+        });
+    })
+
+
 });
