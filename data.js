e1debaeea9641163f0adc0eef3f4f5e764033d5d
MANZANERO JIMENEZ MARIO <mmanji2@mdef.es>
2020-03-04 07:08:00 +0100

    Quitado el proxy

--- data.js
+++ data.js
@@ -1,62 +1,10 @@
 var request = require("request");
 var cheerio = require("cheerio");
 var fs = require('fs');
-/*
-var proxyUrl = "http://" + "mmanji2" + ":" + "Minisdef03" + "@" + "10.7.180.112" + ":" + "80";
-
-var proxiedRequest = request.defaults({ 'proxy': proxyUrl });
-
-proxiedRequest.get("https://www.worldometers.info/coronavirus/", function(err, resp, body) {
-    var $ = cheerio.load(body);
-
-    $.fn.toJson = function() {
-
-        if (!this.is('table')) {
-            return;
-        }
-
-        var results = [],
-            headings = [];
-
-        this.find('thead tr th').each(function(index, value) {
-            headings.push($(value).text());
-        });
-
-
-        this.find('tbody tr').each(function(indx, obj) {
-            var row = {};
-            var tds = $(obj).children('td');
-            headings.forEach(function(key, index) {
-                var value = tds.eq(index).text();
-                row[key] = value;
-            });
-            results.push(row);
-        });
-
-        return results;
-    }
-
-    $(function() {
-        $('#results').html(JSON.stringify($('#excel').toJson(), null, '\t'));
-    });
-
-    var json = $('#main_table_countries').toJson();
-    console.log(json);
-
-    fs.writeFile('data/data.json', JSON.stringify(json), function(err) {
-        if (err) throw err;
-        console.log('Guardado');
-    });
-
-});
-*/
-
-
-
 
 request({
     uri: "https://www.worldometers.info/coronavirus/",
-  }, function(error, response, body) {
+}, function(error, response, body) {
     var $ = cheerio.load(body);
 
     $.fn.toJson = function() {
@@ -95,22 +43,22 @@ request({
     fs.writeFile('data/data.json', JSON.stringify(json), function(err) {
         if (err) throw err;
     });
-  });
+});
 
-  request({
+request({
     uri: "https://www.lavanguardia.com/vida/20200303/473948475682/coronavirus-covid-19-hoy-ultima-hora-espana-cataluna-madrid-sevilla-china-oms-ultimas-noticias-en-directo.html",
-  }, function(error, response, body) {
+}, function(error, response, body) {
     var $ = cheerio.load(body);
     var noticias = $(".story-leaf-body .story-leaf-txt-p .comment");
     var arr = []
-    for(var i = 0; i < noticias.length; i++){
-        arr.push($(noticias[i]).text().replace(/(\r\n|\n|\r)/gm, "").trim().replace(/\s\s+/gm," "))
+    for (var i = 0; i < noticias.length; i++) {
+        arr.push($(noticias[i]).text().replace(/(\r\n|\n|\r)/gm, "").trim().replace(/\s\s+/gm, " "))
     }
     console.log(arr);
 
 
-    fs.writeFile('data/noticias.json',JSON.stringify(arr.splice(0,5)), function(err) {
+    fs.writeFile('data/noticias.json', JSON.stringify(arr.splice(0, 5)), function(err) {
         if (err) throw err;
         console.log('Guardado');
     });
-  });
+});
