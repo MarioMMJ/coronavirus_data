623d066c5fc58ed5b863eeda07f2af72e4e47989
Bcct <xbacost@hotmail.com>
2020-03-05 21:34:59 +0100

    Mejoras en las noticias

--- app.js
+++ app.js
@@ -100,21 +100,21 @@ setInterval(function() {
         let feed1 = await parser.parseURL('https://www.lavanguardia.com/mvc/feed/rss/internacional');
         var json = [];
         feed1.items.forEach(item => {
-            if(item.title.toLowerCase().includes("coronavirus") || item.content.toLowerCase().includes("coronavirus")){
+            if(item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")){
                 json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":new Date(item.pubDate).toLocaleString('en-US',{timeZone:"Europe/Andorra"}),"periodico":"La Vanguardia"});
             }
         });
         
         let feed2 = await parser.parseURL('https://www.abc.es/rss/feeds/abc_ultima.xml');
         feed2.items.forEach(item => {
-            if(item.title.toLowerCase().includes("coronavirus") || item.content.toLowerCase().includes("coronavirus")){
+            if(item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")){
                 json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":new Date(item.pubDate).toLocaleString('en-US',{timeZone:"Europe/Andorra"}),"periodico":"ABC"});
             }
         });
 
         let feed3 = await parser.parseURL('https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada');
         feed3.items.forEach(item => {
-            if(item.title.toLowerCase().includes("coronavirus") || item.content.toLowerCase().includes("coronavirus")){
+            if(item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")){
                 json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":new Date(item.pubDate).toLocaleString('en-US',{timeZone:"Europe/Andorra"}),"periodico":"El Pais"});
             }
         });
--- data.js
+++ data.js
@@ -54,26 +54,26 @@ let parser = new Parser();
 (async () => {
  
     let feed1 = await parser.parseURL('https://www.lavanguardia.com/mvc/feed/rss/internacional');
-    var json = [];
-    feed1.items.forEach(item => {
-        if(item.title.toLowerCase().includes("coronavirus") || item.content.toLowerCase().includes("coronavirus")){
-            json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":item.pubDate,"periodico":"La Vanguardia"});
-        }
-    });
-    
-    let feed2 = await parser.parseURL('https://www.abc.es/rss/feeds/abc_ultima.xml');
-    feed2.items.forEach(item => {
-        if(item.title.toLowerCase().includes("coronavirus") || item.content.toLowerCase().includes("coronavirus")){
-            json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":item.pubDate,"periodico":"ABC"});
-        }
-    });
+        var json = [];
+        feed1.items.forEach(item => {
+            if(item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")){
+                json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":new Date(item.pubDate).toLocaleString('en-US',{timeZone:"Europe/Andorra"}),"periodico":"La Vanguardia"});
+            }
+        });
+        
+        let feed2 = await parser.parseURL('https://www.abc.es/rss/feeds/abc_ultima.xml');
+        feed2.items.forEach(item => {
+            if(item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")){
+                json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":new Date(item.pubDate).toLocaleString('en-US',{timeZone:"Europe/Andorra"}),"periodico":"ABC"});
+            }
+        });
 
-    let feed3 = await parser.parseURL('https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada');
-    feed3.items.forEach(item => {
-        if(item.title.toLowerCase().includes("coronavirus") || item.content.toLowerCase().includes("coronavirus")){
-            json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":item.pubDate,"periodico":"El Pais"});
-        }
-    });
+        let feed3 = await parser.parseURL('https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada');
+        feed3.items.forEach(item => {
+            if(item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")){
+                json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":new Date(item.pubDate).toLocaleString('en-US',{timeZone:"Europe/Andorra"}),"periodico":"El Pais"});
+            }
+        });
 
     fs.writeFile('data/noticias.json', JSON.stringify(json.sort(custom_sort)), function(err) {
         if (err) throw err;
