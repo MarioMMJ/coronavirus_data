dd4117bd597aa107dd8a8b0d39b1a9389530fc64
MANZANERO JIMENEZ MARIO <mmanji2@mdef.es>
2020-03-10 14:09:05 +0100

    Empezamos con la tasa de mortalidad

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ setInterval(function() {
                 var tds = $(obj).children('td');
                 headings.forEach(function(key, index) {
                     var value = tds.eq(index).text();
-                    row[key] = value;
+                    row[key] = value.replace(",", "");;
                 });
                 results.push(row);
             });
@@ -100,22 +100,22 @@ setInterval(function() {
         let feed1 = await parser.parseURL('https://www.lavanguardia.com/mvc/feed/rss/internacional');
         var json = [];
         feed1.items.forEach(item => {
-            if(item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")){
-                json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":new Date(item.pubDate).toLocaleString('en-US',{timeZone:"Europe/Andorra"}),"periodico":"La Vanguardia"});
+            if (item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")) {
+                json.push({ "titulo": item.title, "cuerpo": item.content.replace(/<[^>]*>/g, '').split(".")[0], "link": item.link, "pubDate": new Date(item.pubDate).toLocaleString('en-US', { timeZone: "Europe/Andorra" }), "periodico": "La Vanguardia" });
             }
         });
 
         let feed2 = await parser.parseURL('https://www.abc.es/rss/feeds/abc_ultima.xml');
         feed2.items.forEach(item => {
-            if(item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")){
-                json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":new Date(item.pubDate).toLocaleString('en-US',{timeZone:"Europe/Andorra"}),"periodico":"ABC"});
+            if (item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")) {
+                json.push({ "titulo": item.title, "cuerpo": item.content.replace(/<[^>]*>/g, '').split(".")[0], "link": item.link, "pubDate": new Date(item.pubDate).toLocaleString('en-US', { timeZone: "Europe/Andorra" }), "periodico": "ABC" });
             }
         });
 
         let feed3 = await parser.parseURL('https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada');
         feed3.items.forEach(item => {
-            if(item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")){
-                json.push({"titulo":item.title,"cuerpo":item.content.replace(/<[^>]*>/g, '').split(".")[0],"link":item.link,"pubDate":new Date(item.pubDate).toLocaleString('en-US',{timeZone:"Europe/Andorra"}),"periodico":"El Pais"});
+            if (item.title.toLowerCase().includes("coronavirus") || item.content.split(".")[0].toLowerCase().includes("coronavirus")) {
+                json.push({ "titulo": item.title, "cuerpo": item.content.replace(/<[^>]*>/g, '').split(".")[0], "link": item.link, "pubDate": new Date(item.pubDate).toLocaleString('en-US', { timeZone: "Europe/Andorra" }), "periodico": "El Pais" });
             }
         });
 
